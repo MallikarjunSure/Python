de697eb7415453da00bbf52f038d1a28d2da2889
Mallikarjun <167233499+MallikarjunSure@users.noreply.github.com>
2024-04-22 23:11:31 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -4,18 +4,22 @@ employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-
 print(type(employee))
 print("printing employee data,...")
 print(employee)
+print()
 
 
 #creating an empty dictionary
 Dict={}
 print("Employee Dictionary:")
 print(Dict)
+print()
 
 #creting a dictionary
 #with dict method
 Dict=dict({1:"mango",2:"banana"})
 print("\n Create dictionary by using dict():")
 print(Dict)
+print()
+
 
 #creting a dictionary
 #with each item as a pair
@@ -23,6 +27,7 @@ Dict=dict([(4,'malli'),(2,'shetty')])
 print("\nDictionary with each item as pair")
 print(Dict)
 print(type(Dict))
+print()
 
 
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
@@ -33,6 +38,7 @@ print("Name:%s" %employee["name"])
 print("age:%d" %employee["age"])
 print("salary:%d" %employee["salary"])
 print("dob:%s" %employee["dob"])
+print()
 
 
 #adding elements to dictionary one at a time
@@ -40,12 +46,14 @@ Dict[0]='Kannan'
 Dict[1]='malli'
 Dict[2]='hari'
 print(Dict)
+print()
 
 #with a single key
 #The Emp_ages doesn't to exist dict
 Dict['Emp_ages'] = 20,30,40  #with parenthesisi or without paranthesis it will execute
 print("\n After adding 3 elements...")
 print(Dict)
+print()
 
 
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
@@ -60,6 +68,7 @@ employee["company"]=input("company:");
 employee["x-lover"]=input("x-lover -yes or no:");
 print("printing new data...")
 print(employee)
+print()
 
 
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
@@ -75,7 +84,7 @@ print("deleting the dictionary: Employee")
 del employee
 #print("Let try to print again")
 #print(employee)
-
+print()
 
 
 #create a dictionary
@@ -84,16 +93,20 @@ Dict1={1:'EZTS', 2:'In-plant training', 3:'college'}
 #using pop() method
 pop_key=Dict1.pop(2)
 print(Dict1)
+print()
 
 #for loop to print all the keys of a  dictionary
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
 for x in employee:
     print(x)
+print()
 
 #for loop to print all the values of a  dictionary
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
 for x in employee:
     print(employee[x])
+print()
+
 
 #for loop used to print the items of the dictionary using items()  method
 employee={"name":"balaji","age":70,"salary":200000,"company":"EZTS","dob":"2000-15-15"}
