feecf3c9449ca360097f4df6a8cc179edd6a2256
Mallikarjun <167233499+MallikarjunSure@users.noreply.github.com>
2024-04-22 23:20:44 +0530

    Add files via upload

---
+++ splitmethod.py
@@ -0,0 +1,30 @@
+x,y,z=input('Enetr three numbers:').split()
+print('first number=',x)
+print('second number=',y)
+print('third number=',z)
+print()
+
+a,b,c=input('Enetr three numbers:').split()
+print('first number=',a)
+print('second number=',b)
+print('third number=',c)
+print()
+
+
+p,q,r=input('Enetr three numbers:').split()
+print(p)
+print(q)
+print(r)
+print()
+
+
+m, n =input("Enter a two value: ").split()
+print("First number is {} Second number is {}".format(m, n))
+print()
+
+print('{0} and {1}'.format('Hai','All'))
+print('{1} and {0}'.format('Hai','All'))
+print()
+
+
+print('{0} and {2} and {1}'.format('Hai','All','tack care'))
