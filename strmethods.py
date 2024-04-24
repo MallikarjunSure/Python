17971dc5c7618f18346ca72a970f452e99dadc52
Mallikarjun <167233499+MallikarjunSure@users.noreply.github.com>
2024-04-24 09:32:57 +0530

    Add files via upload

---
+++ strmethods.py
@@ -0,0 +1,10 @@
+st="mallikarjun sure123"
+print(st.isalnum())
+print(st.capitalize())
+print(st.casefold())
+print(st.count('a'))
+print(st.find('3'))
+print(st.index('i'))
+print(st.isalpha())
+print(st.isupper())
+print(st.islower())
