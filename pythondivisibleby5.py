e9982665927abe3e402cb81ea6bdcdd90087a840
Mallikarjun <167233499+MallikarjunSure@users.noreply.github.com>
2024-04-25 09:30:51 +0530

    Update pythondivisibleby5.py

--- pythondivisibleby5.py
+++ pythondivisibleby5.py
@@ -1,6 +1,6 @@
 #1) Write a Python program that accepts a sequence of comma separated 4 digit binary numbers as its input. The program will print the numbers that are divisible by 5 in a comma separated sequence.
-Sample Data : 0100,0011,1010,1001,1100,1001
-Expected Output : 1010
+    #Sample Data : 0100,0011,1010,1001,1100,1001
+    #Expected Output : 1010
 def binary_divisible_by_five(binary_sequence):
     binary_numbers = binary_sequence.split(',')
     divisible_numbers = []
@@ -11,3 +11,4 @@ def binary_divisible_by_five(binary_sequence):
     return ','.join(divisible_numbers)
 binary_sequence = input("Enter a sequence of comma separated 4 digit binary numbers: ")
 print("Numbers divisible by 5:", binary_divisible_by_five(binary_sequence))
+
